c9802d9e8392de650d121e30facb2d435551adf5
yaekobbonke <yaekobbundure@gmail.com>
2023-01-09 09:00:33 +0000

    inheretence completed

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-lookup.py
+"""Defines an instance attribute lookup function."""
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object."""
+    return (list(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+0x0A. Python - Inheritance project 
+Done by Yaekob Bonke
+ALX student cohort 9
