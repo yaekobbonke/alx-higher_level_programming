6b255ebc2efbe6255eb7b8583d1e99f6f36b1f2d
yaekobbonke <yaekobbundure@gmail.com>
2023-01-09 09:13:16 +0000

    classess

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#0-square.py
+"""Defines python empty class"""
+class Square:
+  pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+0x06. Python - Classes and Objects projects 
+Done by Yaekob Bonke(ALX student cohort 9.)
