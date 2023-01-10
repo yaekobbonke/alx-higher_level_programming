6bd28ebabd79a34c75162fe988e0ce6af8c7cce0
yaekobbonke <yaekobbundure@gmail.com>
2023-01-10 09:52:12 +0000

    read file fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 #python i/o project
 """Defines a text file-reading function."""
 
+
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
