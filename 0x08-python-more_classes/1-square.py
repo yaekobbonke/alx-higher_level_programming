746e7cb2de01ef5659389697fcd33a431eda55f9
yaekobbonke <yaekoobbundure@gmail.com>
2023-02-21 17:17:28 -0800

    square

---
+++ 0x08-python-more_classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+    """square class with private size private attribute
+    Attributes:
+    size: private attribute
+    """
+    def __init__(self, size):
+        self.__size = size
