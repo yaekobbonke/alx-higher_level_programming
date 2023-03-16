0aa7dc010f3514778bb50d0ce62e0dc49ab83802
yaekobbonke <yaekobbundure.com>
2023-03-15 21:00:51 -0700

    full discription

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+--  a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+SELECT *
+FROM hbtn_0c_0.first_table;
