9784ca96ef888f032bb8008a5883fe3a1c48409e
yaekobbonke <yaekobbundure.com>
2023-03-15 19:21:44 -0700

    fixed

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,6 +5,4 @@
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT 
-ON hbtn_0d_2
-To user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* To 'user_0d_2'@'localhos2';
