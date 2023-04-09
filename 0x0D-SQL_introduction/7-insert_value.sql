91536f6164e8505e53d0c2172896c5dba0830157
yaekobbonke <yaekobbundure@gmail.com>
2023-04-09 06:42:12 -0700

    new row

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
 -- a script that inserts a new row in the table first_table
-INSERT INTO hbtn_0c_0.first_table(id = 89, name = 'Best School');
+INSERT INTO first_table(id, name) 
+	VALUES
+		(89, 'Best School');
