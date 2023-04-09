822e20b7a7e298caad2739b3224d8587be34f34d
yaekobbonke <yaekobbundure@gmail.com>
2023-04-09 05:36:06 -0700

    first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -4,4 +4,4 @@
 -- The database name will be passed as an argument of the mysql command
 -- If the table first_table already exists, your script should not fail
 -- using  SELECT or SHOW statements is not allowed
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table(id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
