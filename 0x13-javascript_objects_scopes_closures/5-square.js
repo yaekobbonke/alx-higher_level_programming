2085b38a112ff7933579b7484447ba2c400bdc81
yaekobbonke <yaekobbundure@gmail.com>
2023-04-12 19:50:50 -0700

    unfixed

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,35 +1,8 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this;
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let size = 0; size < this.height; size++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-module.exports = class Square extends Rectangle {
+const Rectangle = require('./4-rectangle');
+module.expors = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 };
