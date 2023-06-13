0340888483c1f9fb665eb9645e49d3f8e3633fd8
yaekobbonke <yaekobbundure@gmail.com>
2023-06-13 12:24:30 -0700

    two

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) && (h = parseInt(h))) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(
+      ('X'.repeat(this.width) + '\n')
+        .repeat(this.height)
+        .split('')
+        .slice(0, -1)
+        .join('')
+    );
+  }
+
+  rotate () {
+    this.width += this.height;
+    this.height = this.width - this.height;
+    this.width -= this.height;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
