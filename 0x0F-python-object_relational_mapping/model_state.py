b5edbaff4d95423d30008dfb1536259c94c9aa63
yaekobbonke <yaekobbundure>
2023-06-21 18:46:44 -0700

    updated state class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """Start link class to table in database
 """
-import sys
-from model_state import Base, State
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+class State(Base):
+    """state class definition"""
+    __tablename__ = "states"
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
