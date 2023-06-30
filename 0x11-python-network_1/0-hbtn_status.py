ddba81865682581e8664e7721540b7801869ba89
yaekobbonke <yaekobbundure@gmail.com>
2023-06-30 16:57:14 -0700

    fetches

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    result = response.read()
-    print(result) 
 
-   # if __name__ == "__main__":
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        res = response.read()
+        print("res response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
